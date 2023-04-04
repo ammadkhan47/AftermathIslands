8e7eb2b28fea72eaf230c7350bdc3a31c9b2a9fe
ammadkhan47 <ammadkhanuk@gmail.com>
2023-04-05 02:32:56 +0500

    Update Launch.tsx

--- src/Launch.tsx
+++ src/Launch.tsx
@@ -187,9 +187,10 @@ export const LaunchView: React.FC<LaunchProps> = (props: LaunchProps) => {
                     let data = res.data;
                     let gameCode = data['code'];
                     props.GameCode(gameCode);
-                    if(gameCode.length>0){
+                    /* if(gameCode.length>0){
                     props.Launch();
-                    }
+                    } 
+                    */
                 }
 
             })
